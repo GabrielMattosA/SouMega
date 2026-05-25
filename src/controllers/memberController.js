335d290cb8e8bb6e2f2af704b30c1763b6e911d2
HeitorC333 <heitorcacciatori123@gmail.com>
2026-05-25 13:56:43 -0300

    feat: adicionado tratamento de erros no memberController

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -1,40 +1,94 @@
 import prisma from "../prisma/client.js"
 
 export async function createMember(req, res) {
-  const member = await prisma.member.create({
-    data: req.body
-  })
 
-  res.json(member)
+  try {
+
+    const member = await prisma.member.create({
+      data: req.body
+    })
+
+    res.status(201).json(member)
+
+  } catch(error) {
+
+    if (error.code === "P2002") {
+
+      return res.status(400).json({
+        error: "Email já cadastrado"
+      })
+
+    }
+
+    res.status(500).json({
+      error: "Erro interno do servidor"
+    })
+
+  }
+
 }
-
 export async function getMembers(req, res) {
-  const members = await prisma.member.findMany()
 
-  res.json(members)
+  try {
+
+    const members = await prisma.member.findMany()
+
+    res.json(members)
+
+  } catch(error) {
+
+    res.status(500).json({
+      error: "Erro interno do servidor"
+    })
+
+  }
 }
 
 export async function deleteMember(req, res) {
-  const id = Number(req.params.id)
 
-  await prisma.member.delete({
-    where: {
-      id
-    }
-  })
+  try {
 
-  res.send("Membro deletado")
+    const id = Number(req.params.id)
+
+    await prisma.member.delete({
+      where: {
+        id
+      }
+    })
+
+    res.status(200).json({
+      message: "Membro deletado"
+    })
+
+  } catch(error) {
+
+    res.status(500).json({
+      error: "Erro interno do servidor"
+    })
+
+  }
 }
 
 export async function updateMember(req, res) {
-  const id = Number(req.params.id)
 
-  const member = await prisma.member.update({
-    where: {
-      id
-    },
-    data: req.body
-  })
+  try {
 
-  res.json(member)
+    const id = Number(req.params.id)
+
+    const member = await prisma.member.update({
+      where: {
+        id
+      },
+      data: req.body
+    })
+
+    res.json(member)
+
+  } catch(error) {
+
+    res.status(500).json({
+      error: "Erro interno do servidor"
+    })
+
+  }
 }
