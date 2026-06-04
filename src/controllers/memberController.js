ab775fcc8de691f3a0cb68dfa483e4eb8341d0ae
Diogo-Rs <diogo.santos.rocha4@gmail.com>
2026-06-04 18:42:57 -0300

    Adição da senha no banco de dados e geração auromatica de senha

--- src/controllers/memberController.js
+++ src/controllers/memberController.js
@@ -1,93 +1,79 @@
-import prisma from "../prisma/client.js"
-import { memberSchema } from "../schemas/memberSchema.js"
+import prisma from "../prisma/client.js";
+import { memberSchema } from "../schemas/memberSchema.js";
+import { gerarSenha } from "./passwordgenerate.js";
 
 export async function createMember(req, res) {
 
   try {
-
+    const senha = gerarSenha(req.body.name, req.body.rga)
     const member = await prisma.member.create({
-      data: req.body
-    })
+      data: {
+        req.body,
+        password: senha,
+      } 
+    });
 
-    res.status(201).json(member)
+    res.status(201).json(member);
+  } catch (error) {
+    if (error.code === "P2002") {
+      return res.status(400).json({
+        error: `JÃ¡ existe um membro com esse ${error.meta.target[0]}.`,
+      });
+    }
 
-  } catch(error) {
-
-    const errors = error.issues?.map(err => err.message)
-
-    res.status(400).json({
-      errors
-    })
-
+    res.status(500).json({
+      error: "Erro interno do servidor",
+    });
   }
 
 }
 export async function getMembers(req, res) {
 
   try {
+    const members = await prisma.member.findMany();
 
-    const members = await prisma.member.findMany({
-      include: {
-        projects: true
-      }
-    })
-
-    res.json(members)
-
-  } catch(error) {
-
-    res.status(500).json({
-      error: "Erro interno do servidor"
-    })
-
+    res.json(members);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro interno do servidor" });
   }
 }
 
 export async function deleteMember(req, res) {
-
   try {
-
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
     await prisma.member.delete({
       where: {
-        id
-      }
-    })
+        id,
+      },
+    });
 
     res.status(200).json({
-      message: "Membro deletado"
-    })
-
-  } catch(error) {
-
+      message: "Membro deletado",
+    });
+  } catch (error) {
     res.status(500).json({
-      error: "Erro interno do servidor"
-    })
-
+      error: "Erro interno do servidor",
+    });
   }
 }
 
 export async function updateMember(req, res) {
-
   try {
-
-    const id = Number(req.params.id)
+    const id = Number(req.params.id);
 
     const member = await prisma.member.update({
       where: {
-        id
+        id,
       },
-      data: req.body
-    })
+      data: req.body,
+    });
 
-    res.json(member)
-
-  } catch(error) {
-
+    res.json(member);
+  } catch (error) {
     res.status(500).json({
-      error: "Erro interno do servidor"
-    })
-
+      error: "Erro interno do servidor",
+    });
   }
 }
