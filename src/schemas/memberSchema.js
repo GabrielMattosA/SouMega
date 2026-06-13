82e431bbcb6a72fd07d2b7adadcbcb0b94f90375
HeitorC333 <heitorcacciatori123@gmail.com>
2026-06-13 16:20:16 -0300

    Atualiza memberSchema

--- src/schemas/memberSchema.js
+++ src/schemas/memberSchema.js
@@ -1,10 +1,23 @@
-import { z } from "zod";
+import { z } from "zod"
 
 export const memberSchema = z.object({
-  name: z.string().min(1, "O nome é obrigatório"),
-  email: z.string().email("E-mail inválido"),
-  rga: z.string().min(1, "O RGA é obrigatório"),
+
+  name: z
+    .string()
+    .min(1, "O nome é obrigatório"),
+
+  email: z
+    .string()
+    .email("E-mail inválido"),
+
+  rga: z
+    .string()
+    .min(1, "O RGA é obrigatório"),
+
   cargo: z.string().optional(),
+
   diretoria: z.string().optional(),
-  time: z.string().optional(),
-});
+
+  time: z.string().optional()
+
+})
